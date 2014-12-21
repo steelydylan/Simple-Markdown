e4af943acb0bfab5382bbd6744f2a1ca7cb1a879
steelydylan <ess_president@me.com>
2014-12-21 19:41:11 +0900

    initial commit

---
+++ App/app.js
@@ -0,0 +1,47 @@
+$(function(){
+	var fs = require("fs");
+	var converter = new Showdown.converter();
+	$("#src").on('change keyup paste',function(){
+		var text = $(this).val();
+		var html = converter.makeHtml(text);
+		$("#dist").html(html);
+	});
+	$("#src").keydown(function(e){
+		if (e.keyCode === 9) {
+    		e.preventDefault();
+    		var elem = e.target;
+    		var val = elem.value;
+    		var pos = elem.selectionStart;
+   			elem.value = val.substr(0, pos) + '\t' + val.substr(pos, val.length);
+    		elem.setSelectionRange(pos + 1, pos + 1);
+		}
+	});
+	$("#mdExporter").change(function(e){
+		var path = $(this).val();
+		if(path && fs.existsSync(path)){
+			fs.writeFile(path,$("#src").val(),function(err){
+				if(err){
+					throw err;
+				}
+			});
+		}
+		$(this).val("");
+	});
+	$("#mdImporter").change(function(e){
+		var file = $(this).prop("files")[0];
+		var fr = new FileReader();
+		fr.onload = function(){
+			$("#src").val(fr.result);
+			$("#src").trigger("change");
+		}
+		fr.readAsText(file); 
+	});
+	$(window).keydown(function(e){
+		if(e.keyCode == 83 && e.metaKey){
+			$("#mdExporter").click();
+		}
+		if(e.keyCode == 79 && e.metaKey){
+			$("#mdImporter").click();
+		}
+	});
+});
